daac651e167ebd334c317ae7507ba0c57292426b
somtoc71-collab <somtoc71@gmail.com>
2026-07-26 13:12:43 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -14,20 +14,14 @@ def test() {
 }
 
 def pushAndDeploy() {
-    echo 'Pushing to Docker Hub and deploying application...'
-}
-    
-    
+    echo 'Pushing to Docker Hub and deploying application...'    
     withCredentials([usernamePassword(credentialsId: "${env.DOCKER_HUB_CRED}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "echo \$PASSWORD | docker login -u \$USERNAME --password-stdin"
         sh "docker push ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG}"
         sh "docker push ${env.DOCKER_USER}/${env.IMAGE_NAME}:latest"
-    }
-    
-    
+    }    
     sh "docker stop ${env.CONTAINER_NAME} || true"
     sh "docker rm ${env.CONTAINER_NAME} || true"
     sh "docker run -d --name ${env.CONTAINER_NAME} -p 3000:3000 ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG}"
 }
-
 return this
