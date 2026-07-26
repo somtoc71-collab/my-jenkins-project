d2f610ebeacc9cd68d8b9713d740657a371d7d71
somtoc71-collab <somtoc71@gmail.com>
2026-07-26 13:04:24 +0100

    Refactor Docker commands to use env variables

--- script.groovy
+++ script.groovy
@@ -1,22 +1,33 @@
 def checkout() {
-                echo 'Pulling code from Git repository...'
-            }
-        }
-def build() {
-      echo 'Building Docker image...'
-                sh "docker build -t ${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_TAG} -t ${DOCKER_USER}/${IMAGE_NAME}:latest ."
+    echo 'Pulling code from Git repository...'
+    checkout scm
 }
+
+def build() {
+    echo 'Building Docker image...'
+    sh "docker build -t ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG} -t ${env.DOCKER_USER}/${env.IMAGE_NAME}:latest ."
+}
+
 def test() {
     echo 'Testing the application...'
-    sh "docker run --rm ${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_TAG} npm test"
+    sh "docker run --rm ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG} npm test"
 }
+
 def pushAndDeploy() {
-    echo 'deploying the application...'
-    withCredentials([usernamePassword(credentialsId: "${docker_hub_credential}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh "echo $PASSWORD " | docker login -u $USERNAME --password-stdin"
-        sh "docker push ${DOCKER_USER}/${IMAGE_NAME}:${IMAGE_TAG}: latest"
+    echo 'Pushing to Docker Hub and deploying application...'
+}
     
+    
+    withCredentials([usernamePassword(credentialsId: "${env.DOCKER_HUB_CRED}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        sh "echo \$PASSWORD | docker login -u \$USERNAME --password-stdin"
+        sh "docker push ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG}"
+        sh "docker push ${env.DOCKER_USER}/${env.IMAGE_NAME}:latest"
     }
+    
+    
+    sh "docker stop ${env.CONTAINER_NAME} || true"
+    sh "docker rm ${env.CONTAINER_NAME} || true"
+    sh "docker run -d --name ${env.CONTAINER_NAME} -p 3000:3000 ${env.DOCKER_USER}/${env.IMAGE_NAME}:${env.IMAGE_TAG}"
 }
 
 return this
