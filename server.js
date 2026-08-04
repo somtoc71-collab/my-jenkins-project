0aba0b89562905a328336c5c7b246f537efcb43a
somto collins <somto>
2026-08-05 00:26:30 +0100

    add server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello from Docker EC2!\n');
+});
+
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+});
